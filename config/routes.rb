04f433bfddc8638bca795a79862b4da464f77635
pod5926 <ks.apple.5926@gmail.com>
2020-02-02 23:46:17 +0900

    Make a basic User model (including secure passwords)6章

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'  #新規登録
+  get  '/signup',  to: 'users#new'  #新規登録
 end
