35990d20899dc33ab14dd299da0a4eda9bc4b735
pod5926 <ks.apple.5926@gmail.com>
2020-02-04 10:46:05 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'  #新規登録
+  #7章
+  resources :users
+  post '/signup',  to: 'users#create'
 end
