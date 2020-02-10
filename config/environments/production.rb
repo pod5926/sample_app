ed91e55843ddf1024c3484640c9ebcc8ec39fd74
pod5926 <ks.apple.5926@gmail.com>
2020-02-10 21:44:32 +0900

    Add account activation11章

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-    :tls => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
